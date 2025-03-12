c8c3e3ba26079aa7378e15ff7d5d60a22675b0b4
Filip Dzupin <dzupinf@arcig.cz>
2025-03-13 00:41:27 +0100

    Fixed checksum calculation (calculated from base64)

--- import_images.py
+++ import_images.py
@@ -7,30 +7,36 @@ from abstracts import Card
 
 
 def process_images_to_json(input_picture_directory: str, output_json_file: str) -> None:
-    def get_num_from_name(file: str) -> int:
+    """input: directory of images to be processed;
+        output: json file with images' metadata (key, path, MD5 checksum, base64 encoding of image)
+        :param input_picture_directory:
+        :param output_json_file:
+        :return: None
+    """
+
+    def get_key_from_name(file: str) -> int:
+        """files are named x.png, therefore the 0 index is the x portion of string"""
         return int(os.path.splitext(file)[0])
 
     def encode_to_b64(file: str) -> str:
-        with open(file, "rb") as f:
-            obrazek_base64: str = base64.b64encode(f.read()).decode("utf-8")
-        return obrazek_base64
+        with open(file, "rb") as _f:  #"rb" means read in binary mode
+            b64_image: str = base64.b64encode(_f.read()).decode("utf-8")
+        return b64_image
 
-    def calculate_md5(file: str) -> str:
-        with open(file, "rb") as f:
-            file_data = f.read()
-        return hashlib.md5(file_data).hexdigest()
+    def calculate_md5(encoded_pic: str) -> str:
+        return hashlib.md5(encoded_pic.encode("utf-8")).hexdigest()
 
-    pictures:list[Card] = []
-    files:list[str] = os.listdir(input_picture_directory)
-    sorted_files:list[str] = sorted(files, key=get_num_from_name)
+    pictures: list[Card] = []
+    files: list[str] = os.listdir(input_picture_directory)
+    sorted_files: list[str] = sorted(files, key=get_key_from_name)
 
-    for file in sorted_files:
-        key:int = get_num_from_name(file)
-        path:str = os.path.join(input_picture_directory, file)
-        base64_data:str = encode_to_b64(path)
-        checksum:str = calculate_md5(path)
+    for picture in sorted_files:
+        key: int = get_key_from_name(picture)
+        path: str = os.path.join(input_picture_directory, picture)
+        base64_data: str = encode_to_b64(path)
+        checksum: str = calculate_md5(base64_data)
         pictures.append(Card(key=key, path=path, checksum=checksum, encoded_picture=base64_data))
 
-    json_data:str = json.dumps([picture.model_dump() for picture in pictures], ensure_ascii=False, indent=4) #pydantic
+    json_data: str = json.dumps([picture.model_dump() for picture in pictures], ensure_ascii=False, indent=4)
     with open(output_json_file, "w", encoding="utf-8") as f:
         f.write(json_data)
